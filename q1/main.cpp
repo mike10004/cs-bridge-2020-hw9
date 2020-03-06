e0a5f27c830b35c5e95d86fd1d4d4e4dd557c38b
Mike Chaberski <mac937@nyu.edu>
2020-03-06 18:58:56 -0500

    add comments to functions in q1

--- q1/main.cpp
+++ q1/main.cpp
@@ -13,6 +13,13 @@ const int UPPER_TO_LOWER_OFFSET = 'a' - 'A';
 const int MAX_LINE_LEN = 1024;
 const char DELIM = '\t';
 
+/**
+ * Returns the lower case variant of a letter, if it is an upper case
+ * US English letter.
+ * @param ch a letter
+ * @return lower case version of the given upper-case letter; returns argument
+ * unchanged if it's not an upper-case letter
+ */
 char toLowerCase(char ch) {
     if (ch >= 'A' && ch <= 'Z') {
         return (char) (ch + UPPER_TO_LOWER_OFFSET);
@@ -21,11 +28,22 @@ char toLowerCase(char ch) {
     }
 }
 
+/**
+ * Checks whether a character is a letter in the US English alphabet.
+ * @param ch a character
+ * @return true if and only if the character is a letter in the US English alphabet
+ */
 bool isAlpha(char ch) {
     ch = toLowerCase(ch);
     return ch >= ALPHA_MIN && ch <= ALPHA_MAX;
 }
 
+/**
+ * Increments a letter count. Does nothing if the argument character
+ * is not a letter.
+ * @param letterCounts array of counts, where index 0 represents 'a' and index 25 represents 'z'
+ * @param letter the letter to increment the count of
+ */
 void accumulate(int letterCounts[], char letter) {
     if (isAlpha(letter)) {
         letter = toLowerCase(letter);
@@ -34,6 +52,13 @@ void accumulate(int letterCounts[], char letter) {
     }
 }
 
+/**
+ * Analyzes a line of text. Counts the instances of each letter and returns
+ * the number of words.
+ * @param line a line of text
+ * @param letterCounts array of length ALPHA_SIZE
+ * @return  number of words
+ */
 int analyzeLine(const string& line, int letterCounts[]) {
     bool inWord = false;
     int numWords = 0;
@@ -50,6 +75,11 @@ int analyzeLine(const string& line, int letterCounts[]) {
     return numWords;
 }
 
+/**
+ * Prints a line analysis.
+ * @param letterCounts letter counts array (length ALPHA_SIZE)
+ * @param numWords number of words
+ */
 void printAnalysis(int letterCounts[], int numWords) {
     cout << numWords << DELIM << "words" << endl;
     for (int i = 0; i < ALPHA_SIZE; i++) {
